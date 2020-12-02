ce5212de22c812eaef26e32fbc779dc9d4e9ad4e
CandyCatcher <balaraking@gmail.com>
2020-12-03 02:17:52 +0800

    Communication between web and back

--- foodie-api/src/main/java/top/candysky/controller/HelloController.java
+++ foodie-api/src/main/java/top/candysky/controller/HelloController.java
@@ -1,9 +1,11 @@
 package top.candysky.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
+// 使用这个注解，这个controller就不会在swagger文档中出现了
+@ApiIgnore
 // TODO 两者的区别
 //@Controller
 @RestController
--- foodie-api/src/main/java/top/candysky/controller/PassPortController.java
+++ foodie-api/src/main/java/top/candysky/controller/PassPortController.java
@@ -1,15 +1,17 @@
 package top.candysky.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import top.candysky.pojo.bo.UserBO;
 import top.candysky.service.UserService;
 import top.candysky.utils.IMOOCJSONResult;
 
+// 这个接口可以添加一些内容
+@Api(value = "注册&登录", tags = {"用于注册和登录的相关接口"})
 @RestController
 @RequestMapping("passport")
 public class PassPortController {
@@ -17,6 +19,8 @@ public class PassPortController {
     @Autowired
     UserService userService;
 
+    // 用于阐述当前方法的内容
+    @ApiOperation(value = "用户名是否存在", notes = "用户名是否存在", httpMethod = "GET")
     @GetMapping("/usernameIsExist")
     // @RequestParam说明是请求的参数不是什么url值
     public IMOOCJSONResult usernameIsExist(@RequestParam String username) {
@@ -30,6 +34,39 @@ public class PassPortController {
         if (userService.queryUsernameIsExist(username)) {
             return IMOOCJSONResult.errorMap("用户名已存在");
         }
+
+        return IMOOCJSONResult.ok();
+    }
+
+    @PostMapping("/regist")
+    public IMOOCJSONResult regist(@RequestBody UserBO userBO) {
+
+        String username = userBO.getUsername();
+        String password = userBO.getPassword();
+        String confirmPassword = userBO.getConfirmPassword();
+
+        // 1.判断用户名和密码都不能为空
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(password) || StringUtils.isBlank(confirmPassword)) {
+            return IMOOCJSONResult.errorMsg("用户名和密码都不能为空");
+        }
+
+        // 2.查询用户名是否存在
+        if (userService.queryUsernameIsExist(username)) {
+            return IMOOCJSONResult.errorMsg("用户名已存在");
+        }
+
+        // 3.判断两个密码是否一样
+        if (!password.equals(confirmPassword)) {
+            return IMOOCJSONResult.errorMsg("输入的两次密码不一致");
+        }
+
+        // 4.密码长度不能少于6位
+        if (password.length() < 6) {
+            return IMOOCJSONResult.errorMsg("密码长度小于6位");
+        }
+
+        // 5. 实现注册
+        userService.createUser(userBO);
 
         return IMOOCJSONResult.ok();
     }
